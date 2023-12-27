1f19ea32a999a4be0871780537d2f589aeedf19b
kavya-golamaru <111692021+kavya-golamaru@users.noreply.github.com>
2023-12-27 15:46:30 -0500

    add ondrop method
    add ondrop method

--- src/ValidatedChessboard.tsx
+++ src/ValidatedChessboard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Chessboard from "chessboardjsx";
-import { Chess, Square } from "chess.js"
+import { Chess, Square } from "chess.js";
 
 const ValidatedChessboard = () => {
 
@@ -22,10 +22,51 @@ const ValidatedChessboard = () => {
 
     }
     
+
+    const onDrop = (move_desc: {sourceSquare: string, targetSquare: string, piece: string}): void => {
+
+        var square_legal_moves: Array<string> = chessValidator.moves({square: move_desc.sourceSquare as Square});
+
+        //console.log(square_legal_moves);
+        console.log(move_desc.piece);
+        //console.log(move_desc.targetSquare);
+
+        var legal_move_targets: {[key: string]: string} = {}
+
+        for (var i = 0; i < square_legal_moves.length; i++) {
+
+            square_legal_moves[i].replace('+', '');
+            square_legal_moves[i].replace('#', '');
+
+            if (move_desc.targetSquare == square_legal_moves[i]) {
+
+                var piece_wo_color: string = move_desc.piece.substring(1);
+
+                if (piece_wo_color != 'P') {
+
+                    var target_piece: string = piece_wo_color.concat(move_desc.targetSquare);
+
+                    console.log(target_piece);
+
+                    chessValidator.move(target_piece);
+
+                    return;
+
+                } 
+
+                chessValidator.move(move_desc.targetSquare);
+
+            }
+
+        }
+
+    }
+
     return (
         <Chessboard 
             position="start"
-            allowDrag={allowDrag}/>
+            allowDrag={allowDrag}
+            onDrop={onDrop}/>
     );
 
 }
