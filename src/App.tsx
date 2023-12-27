6743bea58b096e22042040c3a2219026c9cc067e
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-12-26 21:10:00 -0500

    installed stockfishjs, added title to web app
    installed stockfishjs, added title to web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import logoImg from './resources/images/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="title">
+        <img className='logoImg' src={logoImg}/>
+        <h1>Chesso</h1>
+      </div>
+      <div id="main">
+        <div id="chessboard_div">
+
+        </div>
+        <div id="feedback_div">
+
+        </div>
+      </div>
     </div>
   );
 }
