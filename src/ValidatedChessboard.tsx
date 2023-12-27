5b4a941f7f6fc5370f6094d28ff75d129c79e98c
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-12-27 13:34:15 -0500

    committt

---
+++ src/ValidatedChessboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chessboard from "chessboardjsx";
+import { Chess } from "chess.js"
+
+const ValidatedChessboard = () => {
+
+    return (
+        <Chessboard position="start"/>
+    );
+
+}
+
+export default ValidatedChessboard
