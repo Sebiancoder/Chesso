18647cc51d602d46344b4ab5debf5b588b299466
kavya-golamaru <111692021+kavya-golamaru@users.noreply.github.com>
2023-12-28 18:37:35 -0500

    added the complete header functionality

--- src/ChessHeader.tsx
+++ src/ChessHeader.tsx
@@ -16,9 +16,29 @@ const ChessHeader = (props: any) => {
     // }
     return (
         <div>
-            <p id="chessHeader">{
+            <p>{
                 //props.game_state.turn()
-                currTurn == 'w' ? "White to move" : "Black to move"
+                props.game_state.isGameOver() ?
+                props.game_state.isCheckmate() ?
+                currTurn == 'w' ?
+                <p id="black"> Black has checkmated white </p> : <p id="white"> White has checkmated black! </p>
+                : props.game_state.isThreefoldRepetition() ?
+                <p id="gray"> Game drawn due to threefold repetition </p> :
+                props.game_state.isStalemate() ?
+                <p id="gray"> Game drawn due to stalemate </p> :
+                props.game_state.isInsufficientMaterial() ?
+                <p id="gray"> Game drawn due to insufficient material </p> :
+                props.game_state.isDraw() ? <p id="gray"> Game drawn due to 50-move rule </p> :
+                <p id="gray"> Game Over </p> : 
+                currTurn == 'w' ? <p id="white"> White to move </p>: 
+                <p id="black"> Black to move </p>
+
+                // currTurn == 'w' ? "White to move" : props.game_state.isGameOver() 
+                // ? props.game_state.isCheckmate()
+                // ? currTurn == 'w'
+                // ? "White has checkmated black!"
+                // : "Black has checkmated white!"
+                // ? "Game Over!" :  "Black to move"
                 //props.game_state.isGameOver() ? "Game Over!" :  currTurn
             }</p>
         </div>
