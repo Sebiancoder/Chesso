931fbd01ec192aa7b88cc47580291ce183ac8d94
kavya-golamaru <111692021+kavya-golamaru@users.noreply.github.com>
2023-12-28 17:34:17 -0500

    added an element to indicate whose turn it is

--- src/ChessHeader.tsx
+++ src/ChessHeader.tsx
@@ -1,11 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useState} from "react";
+import './ChessHeader.css'
 const ChessHeader = (props: any) => {
 
+    const [currTurn, setCurrTurn] = useState<string>("");
+    useEffect(() => {
+        console.log(currTurn);
+        setCurrTurn(props.game_state.turn())
+    }, [props])
+    // console.log(currTurn);
+    // var curr_player: string = "It's white's turn";
+    // if (currTurn == 'w') {
+    //     curr_player = "It's white's turn";
+    // } else if (currTurn == 'b'){
+    //     curr_player = "It's black's turn";
+    // }
     return (
         <div>
             <p id="chessHeader">{
-                props.game_state.isGameOver()
+                //props.game_state.turn()
+                currTurn == 'w' ? "White to move" : "Black to move"
+                //props.game_state.isGameOver() ? "Game Over!" :  currTurn
             }</p>
         </div>
     )
