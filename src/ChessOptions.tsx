f569734c1f93a05cfa125a1751604d4296eaa8d1
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2024-01-08 09:26:33 -0500

    fixed chessOptions css
    fixed chessOptions css

--- src/ChessOptions.tsx
+++ src/ChessOptions.tsx
@@ -6,9 +6,9 @@ const ChessOptions = (props: any) => {
 
     return (
         <div id="chessOptions">
-            <button id="btn1" onClick={props.onToggleEvalBar}>{props.isVisible ? 'Hide Eval Bar' : 'Show Eval Bar'}</button>
-            <button id="btn2" onClick={props.reset}>Reset Game</button>
-            <button id="undo" onClick={props.undo}>Undo Last Move</button>
+            <button id="btn1" className='coButton' onClick={props.onToggleEvalBar}>{props.isVisible ? 'Hide Eval Bar' : 'Show Eval Bar'}</button>
+            <button id="btn2" className='coButton' onClick={props.reset}>Reset Game</button>
+            <button id="undo" className='coButton' onClick={props.undo}>Undo Last Move</button>
         </div>
     )
 
